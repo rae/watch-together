51d9c800e303be6943dd5e8c08c903275636b4ae
Reid Ellis <rellis@nvidia.com>
2025-03-16 08:09:18 -0400

    Second successful build

--- WatchTogether/WatchTogetherApp.swift
+++ WatchTogether/WatchTogetherApp.swift
@@ -6,17 +6,23 @@
 //
 
 import SwiftUI
+import RealityKit
 
 @main
 struct WatchTogetherApp: App {
-    var body: some Scene {
+    @State private var immersionStyle: ImmersionStyle = .full
+    
+    var body: some SwiftUI.Scene {
+        // Primary window - required for proper scene setup
         WindowGroup {
             ContentView()
         }
         .windowStyle(.volumetric)
         
+        // Immersive space for video environment
         ImmersiveSpace(id: "VideoEnvironment") {
             VideoEnvironmentView()
         }
+        .immersionStyle(selection: $immersionStyle, in: .full, .mixed)
     }
 }
---
+++ WatchTogether/default-binaryarchive.metal
@@ -0,0 +1,9 @@
+//
+//  default-binaryarchive.metal
+//  WatchTogether
+//
+//  Created by Reid Ellis on 2025-03-16.
+//
+
+#include <metal_stdlib>
+using namespace metal;
